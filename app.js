8f15833febc2abcf820dc327fbcae23638b919c6
zhorahmatt <info.rahmathidayat@gmail.com>
2025-05-25 13:06:54 +0800

    learning exploring event modifier

--- app.js
+++ app.js
@@ -19,3 +19,37 @@ const app = Vue.createApp({
 });
 
 app.mount('#user-goal');
+
+
+const appEvent = Vue.createApp({
+    data() {
+        return {
+            counter : 0,
+            name: '',
+        };
+    },
+    methods: {
+        submitForm(event){
+            event.preventDefault(); //this can be change by vue default call like
+            alert(this.name);
+        },
+        //event arguments below are default behavior in javascript
+        //vue just use the behavior to modification the value
+        setName(event, lastName) {
+            this.name = event.target.value + ' ' + lastName;
+        },
+        /**
+         * Add 1 to the counter.
+         */
+/*************  ✨ Windsurf Command ⭐  *************/
+/*******  c2e74a8d-4233-401f-937e-3a56b4bba12d  *******/
+        add() {
+            this.counter += 1;
+        },
+        reduce() {
+            this.counter -= 1;
+        },
+    }
+});
+
+appEvent.mount('#events');
