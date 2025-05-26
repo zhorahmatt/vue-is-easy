b761f85204d761ef20949ff236e89bc7a71b444a
zhorahmatt <info.rahmathidayat@gmail.com>
2025-05-26 15:43:27 +0800

    fix the core functionality of the game

--- project-monster-slayer/app.js
+++ project-monster-slayer/app.js
@@ -14,9 +14,15 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
+            }
             return {width: this.monsterHealth + '%'};
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
+            }
             return {width: this.playerHealth + '%'};
         },
         mayUseSpecialAttack() {
@@ -40,6 +46,12 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        startNewGame() {
+            this.monsterHealth = 100;
+            this.playerHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+        },
         attackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
