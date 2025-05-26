ab826ac39bc676b839c208b16b38e61f89ec02aa
zhorahmatt <info.rahmathidayat@gmail.com>
2025-05-26 15:33:33 +0800

    add logic mechanism to game over

--- project-monster-slayer/app.js
+++ project-monster-slayer/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
             monsterHealth: 100,
             playerHealth: 100,
             currentRound: 0,
+            winner: null,
         }
     },
     computed: {
@@ -20,6 +21,22 @@ const app = Vue.createApp({
         },
         mayUseSpecialAttack() {
             return this.currentRound % 3 === 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
         }
     },
     methods: {
@@ -37,6 +54,17 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(10, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
         }
     }
 });
